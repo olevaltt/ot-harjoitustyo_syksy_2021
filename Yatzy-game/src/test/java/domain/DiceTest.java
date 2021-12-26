1253b1da8abd4b17944dbb0f7bd4705b6a1a1f51
Valtteri Olejniczak <valtteri.olejniczak@outlook.com>
2021-12-26 14:10:13 +0200

    Added picture to dokumentation.

--- Yatzy-game/src/test/java/domain/DiceTest.java
+++ Yatzy-game/src/test/java/domain/DiceTest.java
@@ -1,4 +1,3 @@
-
 package domain;
 
 import static org.junit.Assert.*;
@@ -6,35 +5,29 @@ import org.junit.Test;
 import org.junit.Before;
 import yatzy.domain.Dice;
 
-
-
-public class DiceTest {
-    
+public class DiceTest {    
     Dice testDice;
-    
-    public DiceTest() {
+  
+    @Before
+    public void setUp() {
+        testDice = new Dice();
+
     }
-    
-@Before
-public void setUp() {
-    testDice = new Dice();
-    
-}
-    
-@Test
-public void throwDiceDoesNotReturnNull() {
-    int[] list = new int[]{0,2,4};
-    assertNotEquals(testDice.throwDice(list), null);
-}
 
-@Test
-public void getResultDoesNotReturnNull() {
-    assertNotEquals(testDice.getResult(), null);
-}
+    @Test
+    public void throwDiceDoesNotReturnNull() {
+        int[] list = new int[]{0,2,4};
+        assertNotEquals(testDice.throwDice(list), null);
+    }
 
-@Test
-public void throwAllDiceDoesNotReturnNull() {
-    assertNotEquals(testDice.throwAllDice(), null);
-}
+    @Test
+    public void getResultDoesNotReturnNull() {
+        assertNotEquals(testDice.getResult(), null);
+    }
+
+    @Test
+    public void throwAllDiceDoesNotReturnNull() {
+        assertNotEquals(testDice.throwAllDice(), null);
+    }
 
 }
--- Yatzy-game/src/test/java/domain/PlayerTest.java
+++ Yatzy-game/src/test/java/domain/PlayerTest.java
@@ -1,17 +1,10 @@
 package domain;
 
-
 import yatzy.domain.Player;
-
-
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
-
-public class PlayerTest {
-    
+public class PlayerTest {    
     Player player;
     Player player2;
     
@@ -19,13 +12,12 @@ public class PlayerTest {
         player = new Player(1);
         player2 = new Player(2);
     }
- 
 
-    
     @Test
     public void createdPlayerExists() {
         assertTrue(player!=null);
     }
+    
     @Test
     public void createdPlayer2Exists() {
         assertTrue(player2!=null);
@@ -35,5 +27,4 @@ public class PlayerTest {
     public void toStringIsNotNull() {
         assertNotNull(player.toString());
     }
-
 }
